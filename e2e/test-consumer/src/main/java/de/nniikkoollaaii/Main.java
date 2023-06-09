939fb5631d22c9785e7eadb486fb7d79b5ed335b
Nikolai Seip <nikolai.seip@gmail.com>
2023-06-09 17:51:20 +0000

    ci: add workflow to execute e2e tests

---
+++ e2e/test-consumer/src/main/java/de/nniikkoollaaii/Main.java
@@ -0,0 +1,62 @@
+package io.github.nniikkoollaaii;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class KafkaConsumerApp {
+
+    private static final String TOPIC_NAME = "nniikkoollaaii.topic";
+    private static final String OUTPUT_FILE = "output.txt";
+
+    public static void main(String[] args) {
+        // Kafka consumer configuration
+        Properties props = new Properties();
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "kafka-consumer-group");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put("bootstrap.servers", System.getenv("BOOTSTRAP_SERVERS"));
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        
+        props.put("security.protocol", "SASL_SSL");
+        props.put("sasl.mechanism", "OAUTHBEARER");
+        // Use Workload Identity 
+        props.put("sasl.jaas.config", "org.apache.kafka.common.security.oauthbearer.OAuthBearerLoginModule required;");
+        props.put("sasl.login.callback.handler.class", "io.github.nniikkoollaaii.WorkloadIdentityLoginCallbackHandler");
+
+        // Create the Kafka consumer
+        Consumer<String, String> consumer = new KafkaConsumer<>(props);
+
+        // Subscribe to the Kafka topic
+        consumer.subscribe(Collections.singletonList(TOPIC_NAME));
+
+        // Continuously poll for new records
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+            // Process each record
+            records.forEach(record -> {
+                String event = record.value();
+                System.out.println("Received event: " + event);
+            });
+
+            // Exit the consumer loop after processing the first record
+            if (!records.isEmpty()) {
+                break;
+            }
+        }
+        
+        consumer.close();
+    }
+}
+
---
+++ e2e/test.sh
@@ -0,0 +1,75 @@
+
+cd e2e
+
+echo ""
+echo "Create Kafka cluster"
+echo ""
+docker-compose -f docker-compose.yaml up -d 
+
+echo ""
+echo "Sleep 10s"
+echo ""
+sleep 10s
+
+
+
+echo ""
+echo "Install Admission Webhook Controller in KinD cluster"
+echo ""
+export AZURE_TENANT_ID="f3292839-9228-4d56-a08c-6023c5d71e65"
+docker pull mcr.microsoft.com/oss/azure/workload-identity/webhook:v1.1.0
+kind load docker-image mcr.microsoft.com/oss/azure/workload-identity/webhook:v1.1.0
+curl -sL https://github.com/Azure/azure-workload-identity/releases/download/v1.1.0/azure-wi-webhook.yaml | envsubst | kubectl apply -f -
+
+
+
+echo ""
+echo "Build producer"
+echo ""
+cd test-producer
+mvn package
+docker build -t io.github.nniikkoollaaii.kafka-producer-app:1.0.0 .
+kind load docker-image io.github.nniikkoollaaii.kafka-producer-app:1.0.0
+cd ..
+
+echo ""
+echo "Build consumer"
+echo ""
+cd test-consumer
+mvn package
+docker build -t io.github.nniikkoollaaii.kafka-consumer-app:1.0.0 .
+kind load docker-image io.github.nniikkoollaaii.kafka-consumer-app:1.0.0
+cd ..
+
+
+echo ""
+echo "Create test pods in KinD cluter"
+echo ""
+sed -i 's/{BUILD_NUMBER}/$BUILD_NUMBER/g' manifests/producer.job.yaml
+kubectl apply -f manifests
+
+echo ""
+echo "Sleep 10s"
+echo ""
+sleep 10s
+
+echo ""
+echo "Producer logs: "
+echo ""
+kubectl logs test-producer -n test
+
+echo ""
+echo "Consumer logs: "
+echo ""
+kubectl logs test-consumer -n test
+kubectl logs test-consumer -n test > result-logs.txt
+
+
+
+echo ""
+echo "Checking results ..."
+echo ""
+if [[ "$(cat result-logs.txt | grep "Received")" != "Received event: $BUILD_NUMBER" ]]; then
+  echo "Did not find expected result in log file"; exit 1;
+fi
+echo "Success!"
